afa44f18d53613aef2cb5116033bc298ba227c1a
sachinm78 <sachinm78@gmail.com>
2020-01-23 14:14:11 -0600

    added js mini-drumkit project with styling and sounds; working well, though it mashes together on smaller screens

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+// nav
+
 const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelectorAll('.nav__link')
 
@@ -10,3 +12,24 @@ navLinks.forEach(link => {
         document.body.classList.remove('nav-open');
     })
 })
+
+// drum-kit
+
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    e.target.classList.remove('playing');
+  }
+
+  function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    if (!audio) return;
+
+    key.classList.add('playing');
+    audio.currentTime = 0;
+    audio.play();
+  }
+
+  const keys = Array.from(document.querySelectorAll('.key'));
+  keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+  window.addEventListener('keydown', playSound);
