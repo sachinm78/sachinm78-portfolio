47d239ef85eaa114e04ff222f122ac43e8429278
sachinm78 <sachinm78@gmail.com>
2020-01-24 11:13:18 -0600

    completed shadow section; portfolio-items page looks good on small and large screens

--- js/index.js
+++ js/index.js
@@ -100,3 +100,33 @@ canvas.addEventListener('mousedown', (e) => {
 canvas.addEventListener('mousemove', draw);
 canvas.addEventListener('mouseup', () => isDrawing = false);
 canvas.addEventListener('mouseout', () => isDrawing = false);
+
+
+// mouse shadow
+
+const mouseShadow = document.querySelector('.mouse-shadow');
+const text = mouseShadow.querySelector('h1');
+const walk = 360; // 500px
+
+function shadow(e) {
+    const { offsetWidth: width, offsetHeight: height } = mouseShadow;
+    let { offsetX: x, offsetY: y } = e;
+
+    if (this !== e.target) {
+      x = x + e.target.offsetLeft;
+      y = y + e.target.offsetTop;
+    }
+
+    const xWalk = Math.round((x / width * walk) - (walk / 2));
+    const yWalk = Math.round((y / height * walk) - (walk / 2));
+
+    text.style.textShadow = `
+      ${xWalk}px ${yWalk}px 0 rgba(255,0,0,0.7),
+      ${xWalk * -1}px ${yWalk}px 0 rgba(255,255,255,0.7),
+      ${yWalk}px ${xWalk * -1}px 0 rgba(0,255,0,0.7),
+      ${yWalk * -1}px ${xWalk}px 0 rgba(255,0,255,0.7)
+    `;
+
+}
+
+mouseShadow.addEventListener('mousemove', shadow);
