0e2c5a1e574d59e9f0fe431a016259adda0c8ac9
sachinm78 <sachinm78@gmail.com>
2020-01-23 20:42:12 -0600

    created a new function to set canvas; added button to execute the function; restyled buttons to fix alignment issues

--- js/index.js
+++ js/index.js
@@ -38,7 +38,7 @@ function removeTransition(e) {
 const canvas = document.querySelector('#draw');
 const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+canvas.height = 500;
 ctx.strokeStyle = '#BADA55';
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
@@ -53,7 +53,7 @@ let direction = true;
 
 function draw(e) {
   if (!isDrawing) return; // stop the fn from running when they are not moused down
-  console.log(e);
+//   console.log(e);
   ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
   ctx.beginPath();
   // start from
@@ -82,9 +82,12 @@ function draw(e) {
 
 }
 
+function setCanvas() {
+    ctx.fillStyle="linen"
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
+}
+
 function clearGrid() {
-    // ctx.fillStyle="lightgray"
-    // ctx.fillRect(0, 0, canvas.width, canvas.height)
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
 
